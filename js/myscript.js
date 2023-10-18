6786d048b6e86ae893d328a529ac01a1e0efbaad
EmilianoAprile <emilianoaprile77@icloud.com>
2023-10-18 15:32:07 +0200

    ADDED - MILESTONE 1: for cicle members info

--- js/myscript.js
+++ js/myscript.js
@@ -4,40 +4,57 @@ console.log('OUR TEAM')
 
 const team = [
     {
-      name: "Wayne Barnett",
-      role: "Founder & CEO",
-      photo: "img/wayne-barnett-founder-ceo.jpg",
+        name: "Wayne Barnett",
+        role: "Founder & CEO",
+        photo: "img/wayne-barnett-founder-ceo.jpg",
     },
 
     {
-      name: "Angela Caroll",
-      role: "Chief Editor",
-      photo: "img/angela-caroll-chief-editor.jpg",
+        name: "Angela Caroll",
+        role: "Chief Editor",
+        photo: "img/angela-caroll-chief-editor.jpg",
     },
 
     {
-      name: "Walter Gordon",
-      role: "Office Manager",
-      photo: "img/walter-gordon-office-manager.jpg",
+        name: "Walter Gordon",
+        role: "Office Manager",
+        photo: "img/walter-gordon-office-manager.jpg",
     },
 
     {
-      name: "Angela Lopez",
-      role: "Social Media Manager",
-      photo: "img/angela-lopez-social-media-manager.jpg",
+        name: "Angela Lopez",
+        role: "Social Media Manager",
+        photo: "img/angela-lopez-social-media-manager.jpg",
     },
 
     {
-      name: "Scott Estrada",
-      role: "Developer",
-      photo: "img/scott-estrada-developer.jpg",
+        name: "Scott Estrada",
+        role: "Developer",
+        photo: "img/scott-estrada-developer.jpg",
     },
 
     {
-      name: "Barbara Ramos",
-      role: "Graphic Designer",
-      photo: "img/barbara-ramos-graphic-designer.jpg",
+        name: "Barbara Ramos",
+        role: "Graphic Designer",
+        photo: "img/barbara-ramos-graphic-designer.jpg",
     }
-  ]
+]
 
-  console.log(team);
+console.log(team);
+
+// MILESTONE 1
+
+for (let i = 0; i < team.length; i++) {
+
+    console.log(i);
+
+    const informazioniMembriTeam = team[i];
+    console.log(informazioniMembriTeam);
+
+    console.log('Name:', informazioniMembriTeam.name);
+
+    console.log('Role:', informazioniMembriTeam.role);
+
+    console.log('Photo:', informazioniMembriTeam.photo);
+}
+
